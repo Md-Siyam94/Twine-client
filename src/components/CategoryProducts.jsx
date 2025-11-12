f62a61f09ad6dbdc35a24c27b33860a539f0727b
Md-Siyam94 <siyam942404.com>
2025-11-12 21:16:30 +0600

    successfully add a product in cart

--- src/components/CategoryProducts.jsx
+++ src/components/CategoryProducts.jsx
@@ -13,11 +13,10 @@ import useCategoryWiseProducts from '../hooks/useCategoryWiseProducts';
 
 const CategoryProducts = ({category}) => {
     const products = useCategoryWiseProducts(category)
+  
 
 
-    const handleAddToCart = (id)=>{
-        console.log(id);
-    }
+    
 
     return (
         <div className=' py-5 w-11/12 mx-auto '>
@@ -49,9 +48,9 @@ const CategoryProducts = ({category}) => {
                      {/* <p className=" text-sm">Price: <span className='text-md font-semibold'>{product?.price} {product?.currency}</span></p> */}
                     <p className='text-sm'>Brand: <span className='text-md font-semibold'>{product?.brand}</span></p>
                     <p className=' opacity-60'>{product?.description?.slice(0,70)}...more</p>
-                    <div className="card-actions justify-between">
-                        <Link to={`/product-details/${product?._id}`} className=" border-b hover:text-teal-400">View Details</Link>
-                        <button type='button' onClick={()=>handleAddToCart(product?._id)}  className="py-1 px-4 rounded-2xl bg-teal-400 hover:bg-teal-600 hover:text-white">Add to Cart</button>
+                    <div className="card-actions justify-end">
+                        <Link to={`/product-details/${product?._id}`} className=" border-b border-r py-1 px-4 rounded-2xl hover:text-green-400">View Details</Link>
+                       
                     </div>
                 </div>
             </div>
--- src/pages/Product Details/ProductDetails.jsx
+++ src/pages/Product Details/ProductDetails.jsx
@@ -2,16 +2,62 @@ import { FaRegHeart } from "react-icons/fa";
 import { RiShoppingCartLine } from "react-icons/ri";
 import { Link, useLoaderData, useParams } from "react-router-dom";
 import CategoryProducts from "../../components/CategoryProducts";
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 
 const ProductDetails = () => {
     const params = useParams()
     const product = useLoaderData()
-    // console.log(product);
+    const { user } = useContext(AuthContext)
+    const axiosPublic = useAxiosPublic()
     const { name, description, price, _id, currency, size, color, inStock, material, brand, image, category } = product || {}
 
     const singleSize = size.join(", ")
     const singleColor = color.join(", ")
+
+
+    const handleAddToCart = (id) => {
+        const productInfo = {
+            userEmail: user?.email,
+            userName: user?.displayName,
+            productName: name,
+            productId: _id,
+            productImage: image,
+            description: description,
+            price: price,
+            size: size,
+            color: color,
+
+        }
+        console.log(productInfo);
+        axiosPublic.post("/cart_products", productInfo)
+            .then(res => {
+                console.log(res?.data);
+                if (res?.data?.success) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Product has been added to cart",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }else{
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "error",
+                        title: `${res?.data?.message}`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+            .catch(err => {
+                console.log("error msg", err);
+            })
+    }
     return (
         <div className="w-10/12 py-14  mx-auto mt-5 rounded-2xl shadow grid grid-cols-12 ">
             <div className="flex  bg-base-100 col-span-9 py-4 shadow-sm">
@@ -28,21 +74,21 @@ const ProductDetails = () => {
                     </h2>
                     <p className=" my-4 text-gray-500">Brand: <span className="text-blue-500  ">{brand}</span></p>
                     <p className=" my-4 text-gray-500">Category: <span className="text-blue-500  ">{category}</span></p>
-                  
+
                     <p className="my-4">{inStock ? <div className="text-green-500">In Stock</div> : <div className="text-red-500">Not available</div>}</p>
                     <p className="flex  gap-2"> Size: {singleSize}
                     </p>
-                    <p className="flex gap-2 my-4">color: { singleColor }
+                    <p className="flex gap-2 my-4">color: {singleColor}
                     </p>
                     <p>TK. <span className="text-lg font-semibold">{price} {currency}</span></p>
                     <div className="my-6 flex gap-4">
-                       <button type="button"  className="py-3 px-10 rounded-lg bg-teal-400 hover:bg-teal-500 hover:text-white flex gap-2">Add to Cart<RiShoppingCartLine className='text-2xl' /></button>
-                       <button type="button"  className="py-3 px-10 rounded-lg bg-teal-400 hover:bg-teal-500 hover:text-white flex gap-2">Add to Wishlist<FaRegHeart className="text-2xl" /></button>
+                        <button onClick={() => handleAddToCart(_id)} type="button" className="py-3 px-10 rounded-lg bg-teal-400 hover:bg-teal-500 hover:text-white flex gap-2">Add to Cart<RiShoppingCartLine className='text-2xl' /></button>
+                        <button type="button" className="py-3 px-10 rounded-lg bg-teal-400 hover:bg-teal-500 hover:text-white flex gap-2">Add to Wishlist<FaRegHeart className="text-2xl" /></button>
                     </div>
                 </div>
             </div>
             <div className="col-span-3 ">
-        {/* <CategoryProducts category={category}></CategoryProducts> */}
+                {/* <CategoryProducts category={category}></CategoryProducts> */}
             </div>
         </div>
     );
