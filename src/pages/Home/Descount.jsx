2aac16e47480d1865083ccada5a0405b8b8ef65a
Md-Siyam94 <mdsiyam9424@gmail.com>
2026-07-14 00:42:56 +0600

    update descount page

--- src/pages/Home/Descount.jsx
+++ src/pages/Home/Descount.jsx
@@ -3,9 +3,9 @@ import descount2 from "../../assets/descount-2.webp"
 
 const Descount = () => {
     return (
-        <div className="flex justify-evenly">
-            <img className="h-40 w-60 object-cover" src={descount1} alt="Descount image " />
-            <img className="h-40 w-60 object-cover" src={descount2} alt="Descount image" />
+        <div className="py-24 max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 justify-end items-center ">
+            <img className="w-96 object-cover" src={descount1} alt="Descount image " />
+            <img className="w-96 object-cover" src={descount2} alt="Descount image" />
         </div>
     );
 };
