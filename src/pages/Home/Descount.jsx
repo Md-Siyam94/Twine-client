6eab5c078496fd247eba343a5cc4f0120c4b18f5
Md-Siyam94 <mdsiyam9424@gmail.com>
2026-08-03 20:05:48 +0600

    update descout banner section

--- src/pages/Home/Descount.jsx
+++ src/pages/Home/Descount.jsx
@@ -1,11 +1,11 @@
-import descount1 from "../../assets/descount-1.webp"
+import descount1 from "../../assets/descount banner.webp"
 import descount2 from "../../assets/descount-2.webp"
 
 const Descount = () => {
     return (
-        <div className="py-24 max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 justify-end items-center ">
-            <img className="w-96 object-cover" src={descount1} alt="Descount image " />
-            <img className="w-96 object-cover" src={descount2} alt="Descount image" />
+        <div className="py-24 max-w-7xl mx-auto  ">
+            <img className="object-cover" src={descount1} alt="Descount image " />
+            {/* <img className="w-96 object-cover" src={descount2} alt="Descount image" /> */}
         </div>
     );
 };
