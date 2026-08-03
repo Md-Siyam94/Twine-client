e8c94fe500f8c5df7a601bf5c3e2df194d126eed
Md-Siyam94 <mdsiyam9424@gmail.com>
2026-08-03 19:37:05 +0600

    update cetegory section

--- src/components/shared/AdminLogin.jsx
+++ src/components/shared/AdminLogin.jsx
@@ -4,25 +4,25 @@ import { useNavigate } from 'react-router-dom';
 import { RiAdminLine } from 'react-icons/ri';
 
 const AdminLogin = () => {
-    const {signIn} = useContext(AuthContext)
+    const { signIn } = useContext(AuthContext)
     const navigate = useNavigate()
-    const hanldeAdminLogin =()=>{
+    const hanldeAdminLogin = () => {
         const email = 'admin@gmail.com';
         const password = 'admin123456';
 
         signIn(email, password)
-        .then(()=>{
-            navigate("/")
-        })
-        .catch(err=>{
-           console.log(err);
-        })
+            .then(() => {
+                navigate("/")
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
     return (
         <button onClick={hanldeAdminLogin} className="btn w-full flex gap-2 items-center hover:bg-sky-200  border bg-white text-black ">
-               <RiAdminLine className='text-xl text-green-500'/>
-                Admin Login
-            </button>
+            <RiAdminLine className='text-xl text-green-500' />
+            Admin Login
+        </button>
     );
 };
 
